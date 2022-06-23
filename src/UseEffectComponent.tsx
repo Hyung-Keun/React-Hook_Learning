cd6d3d3c8faa102e68ec3fb22629cf82a6d31d61
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-06-23 22:18:47 +0900

    Chore: useEffect컴포넌트 내 불필요한 코드 삭제.

--- src/UseEffectComponent.tsx
+++ src/UseEffectComponent.tsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
-import { validateLocaleAndSetLanguage } from "typescript";
 
 function UseEffectComponent() {
   const [val, setVal] = useState(1);
 
   useEffect(() => {
     const timer = window.setInterval(() => {
+      console.log("Rendering!");
       setVal((v) => v + 1);
     }, 1000);
+
     return () => window.clearInterval(timer);
   }, []);
   return <div>{val}</div>;
