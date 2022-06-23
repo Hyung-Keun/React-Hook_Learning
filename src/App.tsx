0e4a596164f2128d598808b3f2398fa249d16d50
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-06-23 16:16:12 +0900

    Feature: useEffect 컴포넌트 추가.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import UseEffectComponent from "./UseEffectComponent";
 import UseStateComponent from "./UseStateComponent";
 
 function App() {
@@ -5,6 +6,8 @@ function App() {
     <div>
       <h1>useState</h1>
       <UseStateComponent />
+      <h1>useEffect</h1>
+      <UseEffectComponent />
     </div>
   );
 }
