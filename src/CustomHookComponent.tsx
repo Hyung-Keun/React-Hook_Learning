a2c0d307e268779c1c385df31526be105ad5a7b6
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-06-24 19:13:04 +0900

    Feature: useMemo기능 추가.

--- src/CustomHookComponent.tsx
+++ src/CustomHookComponent.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 export interface Beverage {
   name: string;
   producerName: string;
@@ -32,6 +32,18 @@ function useFetchData(url: string): {
 
 function CustomHookComponent() {
   const { data, done } = useFetchData("/hv-taplist.json");
-  return <div>{done && <img src={data![0].logo} alt="Beverage logo" />}</div>;
+  const portlandTaps = useMemo(
+    () =>
+      (data || []).filter((bev) => bev.producerLocation.includes("Portland")),
+    [data]
+  );
+
+  return (
+    <div>
+      {portlandTaps.length && (
+        <img src={portlandTaps![0].logo} alt="Beverage logo" />
+      )}
+    </div>
+  );
 }
 export default CustomHookComponent;
